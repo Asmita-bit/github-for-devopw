0040db686a4301ac3ff252f361b1d7a7b745d080
Asmita Gupta <asmi.gupta071@gmail.com>
2025-06-08 06:41:54 +0000

    module decstring added

--- testing.py
+++ testing.py
@@ -1,7 +1,11 @@
+"""This module performs a simple addition operation using a sample function."""
+
 def my_function():
+    """Returns the sum of two predefined numbers."""
     a = 5
     b = 6
-    return a+b
+    return a + b
 
-
+# Call the function (optional in production, but okay for test)
 my_function()
+
